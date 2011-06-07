bef11057c80a0b146ba142ee89833cf8fb9cb60f
root <root@localhost.localdomain>
2011-06-07 18:57:43 +0700

    modified module-default controllers:index

--- app/modules/default/controllers/IndexController.php
+++ app/modules/default/controllers/IndexController.php
@@ -9,6 +9,10 @@ class IndexController extends Zend_Controller_Action
 	public function __call($method, $args)
 	{
 		$tw = Zend_Registry::get('twurfl');
+		$tw->GetDeviceCapabilitiesFromAgent($_SERVER['HTTP_USER_AGENT'],true);
+		$cap = $tw->capabilities;
+		
+		/*
         if(!$tw->getDeviceCapability("is_wireless_device")){
         	$this->_forward('index','index','hol-site');
        	}
@@ -19,6 +23,23 @@ class IndexController extends Zend_Controller_Action
 		    $cdn = $config->getOption('mobile');
 			$this->_redirect($cdn['url']);	        
        	}
+       	*/
+       	
+		// check if this device is mobile
+		if($cap['product_info']['is_wireless_device']){
+			
+		    $registry = Zend_Registry::getInstance();
+		    $config = $registry->get(Pandamp_Keys::REGISTRY_APP_OBJECT);
+		    $cdn = $config->getOption('mobile');
+			$this->_redirect($cdn['url']);	        
+			
+		}else{
+			
+			$this->_forward('index','index','hol-site');			
+			
+		}
+       	
+       	
 	}
 }
 
