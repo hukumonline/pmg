6f505150adbe9a2a00ab79c958e036af36e0dcab
nihki <nihki@hukumonline.com>
2011-07-06 17:38:35 +0700

    modified mobile url

--- app/modules/default/controllers/IndexController.php
+++ app/modules/default/controllers/IndexController.php
@@ -12,26 +12,16 @@ class IndexController extends Zend_Controller_Action
 		$tw->GetDeviceCapabilitiesFromAgent($_SERVER['HTTP_USER_AGENT'],true);
 		$cap = $tw->capabilities;
 		
-		/*
-        if(!$tw->getDeviceCapability("is_wireless_device")){
-        	$this->_forward('index','index','hol-site');
-       	}
-        else
-        {
-		    $registry = Zend_Registry::getInstance();
-		    $config = $registry->get(Pandamp_Keys::REGISTRY_APP_OBJECT);
-		    $cdn = $config->getOption('mobile');
-			$this->_redirect($cdn['url']);	        
-       	}
-       	*/
-       	
 		// check if this device is mobile
 		if($cap['product_info']['is_wireless_device']){
 			
 		    $registry = Zend_Registry::getInstance();
 		    $config = $registry->get(Pandamp_Keys::REGISTRY_APP_OBJECT);
 		    $cdn = $config->getOption('mobile');
-			$this->_redirect($cdn['url']);	        
+		    
+		    $uri = $_SERVER['REQUEST_URI'];
+		    
+			header("Location:".$cdn['url'].$uri);
 			
 		}else{
 			
--- app/modules/hol-site/controllers/BrowserController.php
+++ app/modules/hol-site/controllers/BrowserController.php
@@ -13,11 +13,31 @@ class HolSite_BrowserController extends Zend_Controller_Action
     }
     function detailAction()
     {
-    	$this->_helper->layout->setLayout('layout-detail');
-        $catalogGuid = ($this->_getParam('guid'))? $this->_getParam('guid') : '';
-        $page = ($this->_getParam('page'))? $this->_getParam('page') : '';
-        $this->view->catalogGuid = $catalogGuid;
-        $this->view->page = $page;
+		$tw = Zend_Registry::get('twurfl');
+		$tw->GetDeviceCapabilitiesFromAgent($_SERVER['HTTP_USER_AGENT'],true);
+		$cap = $tw->capabilities;
+		
+		// check if this device is mobile
+		if($cap['product_info']['is_wireless_device']){
+			
+		    $registry = Zend_Registry::getInstance();
+		    $config = $registry->get(Pandamp_Keys::REGISTRY_APP_OBJECT);
+		    $cdn = $config->getOption('mobile');
+		    
+		    $uri = $_SERVER['REQUEST_URI'];
+		    
+			header("Location:".$cdn['url'].$uri);
+			
+		}else{
+			
+	    	$this->_helper->layout->setLayout('layout-detail');
+	        $catalogGuid = ($this->_getParam('guid'))? $this->_getParam('guid') : '';
+	        $page = ($this->_getParam('page'))? $this->_getParam('page') : '';
+	        $this->view->catalogGuid = $catalogGuid;
+	        $this->view->page = $page;
+			
+		}
+       	
     }
     function detailIssueAction()
     {
