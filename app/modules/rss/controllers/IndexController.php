7995df38222dbe87028aa25546e3d45ce51cdec3
unknown <kiky@.(none)>
2011-08-04 19:02:25 +0700

    modified index rss

--- app/modules/rss/controllers/IndexController.php
+++ app/modules/rss/controllers/IndexController.php
@@ -11,34 +11,66 @@ class Rss_IndexController extends Zend_Controller_Action
 		$this->_helper->layout->disableLayout();
 		$this->getHelper('viewRenderer')->setNoRender(true); 
 		
+	    $registry = Zend_Registry::getInstance();
+	    $config = $registry->get(Pandamp_Keys::REGISTRY_APP_OBJECT);
+	    $cdn = $config->getOption('cdn');
+	    
+	    $sDir = $cdn['static']['url']['images'];
+	    
+	    $content = 0;
+	    $entries = array();
+	    
 		$rowset = App_Model_Show_Catalog::show()->fetchFromFolder('lt4aaa29322bdbb',0,10);
 		foreach ($rowset as $row)
 		{
-			$entries[] = array(
-				'title' 		=> App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($row['guid'],'fixedTitle'),
-				'link'			=> ROOT_URL.'/berita/baca/'.$row['guid'].'/'.$row['shortTitle'],
-				'description'	=> $this->limitword(2,App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($row['guid'],'fixedContent')),
-				'lastUpdate'	=> strtotime($row['publishedDate'])
-			);
+		    $rowsetRelatedItem = App_Model_Show_RelatedItem::show()->getDocumentById($row['guid'],'RELATED_IMAGE');
+		    $itemGuid = (isset($rowsetRelatedItem['itemGuid']))? $rowsetRelatedItem['itemGuid'] : '';
+			$chkDir = $sDir."/".$row['guid']."/".$itemGuid;
+			if (@getimagesize($chkDir))
+			{
+				$pict = $sDir ."/". $row['guid'] ."/". $itemGuid;
+			}
+			else
+			{
+				$pict = $sDir ."/". $itemGuid;
+			}
+			
+			
+			if (Pandamp_Lib_Formater::thumb_exists($pict . ".jpg")) { $thumb = $pict . ".jpg"; }
+			if (Pandamp_Lib_Formater::thumb_exists($pict . ".gif")) { $thumb = $pict . ".gif"; }
+			if (Pandamp_Lib_Formater::thumb_exists($pict . ".png")) { $thumb = $pict . ".png"; }
+
+			$entries[$content]['title'] 		= App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($row['guid'],'fixedTitle');
+			$entries[$content]['link']			= ROOT_URL.'/berita/baca/'.$row['guid'].'/'.$row['shortTitle'];
+			$entries[$content]['guid']			= ROOT_URL.'/berita/baca/'.$row['guid'].'/'.$row['shortTitle'];
+			$entries[$content]['pubDate']		= strtotime($row['publishedDate']);
+			
+			if (!empty($thumb)) {
+				$d = "<img src=\"".$thumb."\" align=\"left\" hspace=\"7\" width=\"100\" />".$this->limitword(2,App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($row['guid'],'fixedContent'));	
+				$entries[$content]['description']	= $d;
+				$size = getimagesize($thumb);
+				$entries[$content]['enclosure'] = array(
+					array(
+						'url' 		=> $thumb,
+						'type'		=> $size['mime'],
+						'length'	=> '10240'
+					)
+				);
+			}
+			else 
+			{
+				$d = $this->limitword(2,App_Model_Show_CatalogAttribute::show()->getCatalogAttributeValue($row['guid'],'fixedContent'));
+				$entries[$content]['description']	= $d;
+			}
+		
+			$content++;
 		}
 		
-//	    $registry = Zend_Registry::getInstance();
-//	    $config = $registry->get(Pandamp_Keys::REGISTRY_APP_OBJECT);
-//	    $cdn = $config->getOption('cdn');
-	    
-//		$img = array('link'=>'ff','url'=>'http://rss.detik.com/images/rsslogo_detiknews.gif','title'=>'HukumonlineBerita');
-		
 		$feed = Zend_Feed::importArray(array(
-	        'title'   		=> 'RSS Feed Hukumonline',
+	        'title'   		=> 'Hukumonline Warta',
 	        'link'    		=> ROOT_URL,
 	        'description'	=> 'hukumonline.com sindikasi',
-	        'image'			=> array(
-	        	array(
-	        	'link'=>'ff',
-	        	'url'=>'http://rss.detik.com/images/rsslogo_detiknews.gif',
-	        	'title'=>'HukumonlineBerita'
-	        	)
-	        ),
+	        'image'			=> $cdn['static']['images'].'/logo.gif',
 	        'charset' 		=> 'UTF-8',
 	        'entries' 		=> $entries
 		), 'rss');
